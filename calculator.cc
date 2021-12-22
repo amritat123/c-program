ce3b1d045b8a3cd0ead034ea6a4ccbcaf5290f71
Amrita Tiwari <83055726+amritat123@users.noreply.github.com>
2021-12-22 23:49:46 +0530

    Add files via upload

---
+++ calculator.cc
@@ -0,0 +1,37 @@
+# include <iostream>
+using namespace std;
+
+int main(){
+    char op;
+    int  num1, num2;
+
+    cout << "enter any opertor";
+    cin >> op;
+
+    cout << "Enter two numbers:-";
+    cin >> num1 >> num2;
+
+    switch(op) {
+        case '+':
+            cout << num1 << " + " << num2 << " = " << num1 + num2;
+            break;
+
+        case '-':
+            cout << num1 << " - " << num2 << " = " << num1 - num2;
+            break;
+
+        case '*':
+            cout << num1 << " * " << num2 << " = " << num1 * num2;
+            break;
+
+        case '/':
+            cout << num1 << " / " << num2 << " = " << num1 / num2;
+            break;
+
+        default:
+            cout << "Error! operator is not correct";
+            break;
+    }
+
+    return 0;
+}
