f15f69b22e8fad9c9bc2d7c9ce9344ab58bcef9d
Amrita Tiwari <83055726+amritat123@users.noreply.github.com>
2021-12-22 23:57:30 +0530

    Add files via upload

---
+++ sum_of_number.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+int main(){
+    int number,sum=0,count=0;
+    cout<<"enter your number";
+    cin >> number;
+
+    while (number>=count)
+         if (count%2==0)
+        sum=sum+count;
+        count++;
+    cout<< sum;
+    return 0;
+
+}
