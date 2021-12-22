bb9ae40351bf86f3d8285665d6e25e24d07afa4e
Amrita Tiwari <83055726+amritat123@users.noreply.github.com>
2021-12-23 00:04:19 +0530

    Add files via upload

---
+++ maximum_minimum.cpp
@@ -0,0 +1,21 @@
+// #include<iostream>
+// using namespace std;
+// int main(){
+//     int num1,num2,num3,max;
+//     cout<< "please enter the num1:-";
+//     cin>> num1;
+//     cout<< "please enter the num2:-;";
+//     cin>>num2;
+//     cout<< "please enter the num3:-";
+//     cin>> num3;
+
+//     if (num1>num2 && num1 >num3)
+//         cout<< num1<<"is greater";
+//     else if (num2>num1 && num2> num3)
+//         cout<< num2<< "is greater";
+//     else
+//         cout<< num3<< "is greater";
+
+//     return 0;
+
+// }
