a01094fb6007b4de231617c5092efe4b743c8af9
Amrita Tiwari <83055726+amritat123@users.noreply.github.com>
2021-12-22 23:49:29 +0530

    Add files via upload

---
+++ even_odd_number.cpp
@@ -0,0 +1,13 @@
+//checking number is even or odd number in if else...
+#include <iostream>
+using namespace std;
+int main (){
+    int num ;
+    cout << "please enter any number:-";
+    cin >>num;
+    if (num%2==0)
+        cout<<num<<"it is even nubmer ";
+    else
+        cout<<num<<"it's not even number";
+    return 0;
+}
